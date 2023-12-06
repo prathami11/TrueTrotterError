6a963e59f4850f96c42c39862013f63f6bfdf890
prathami11 <72123148+prathami11@users.noreply.github.com>
2023-12-06 06:43:33 +0530

    Update shrink_ham.py

--- CSA-master/CSA-master/shrink_ham.py
+++ CSA-master/CSA-master/shrink_ham.py
@@ -7,18 +7,20 @@ import saveload_utils as sl #From CSA library
 from scipy import sparse
 import os
 import pickle
-h_ferm=sl.load_fermionic_hamiltonian('beh2',path_prefix) #loads the Openfermion Fermion operator corresponding to the
+mol = "beh2" # options: h2, lih, beh2, h2o, nh3
+h_ferm=sl.load_fermionic_hamiltonian(mol,path_prefix) #loads the Openfermion Fermion operator corresponding to the
                                                       #molecule mol
-
-Sz_op = of.linalg.get_sparse_operator(of.hamiltonians.sz_operator(7))
-Sx_op = of.linalg.get_sparse_operator(of.hamiltonians.sx_operator(7))
-Sy_op = of.linalg.get_sparse_operator(of.hamiltonians.sy_operator(7))
+n_qubits = openfermion.count_qubits(h_ferm) # no. of qubits
+Sz_op = of.linalg.get_sparse_operator(of.hamiltonians.sz_operator(n_qubits/2))
+Sx_op = of.linalg.get_sparse_operator(of.hamiltonians.sx_operator(n_qubits/2))
+Sy_op = of.linalg.get_sparse_operator(of.hamiltonians.sy_operator(n_qubits/2))
 # S squared operator
 S_sq = Sx_op**2+Sy_op**2+Sz_op**2
-n_op = of.linalg.get_sparse_operator(of.hamiltonians.number_operator(14),14) 
+n_op = of.linalg.get_sparse_operator(of.hamiltonians.number_operator(n_qubits),n_qubits) 
 print("exponentiating")
 import scipy as sp
-fn =sp.sparse.linalg.expm(-1e8*((n_op-6*sp.sparse.identity(2**14))**2))
+n_elec = 6 # no. of ground state electrons of the respective molecule, modify accordingly
+fn =sp.sparse.linalg.expm(-1e8*((n_op-n_elec*sp.sparse.identity(2**n_qubit))**2))
 fsq =sp.sparse.linalg.expm(-1e8*((S_sq)**2))
 fz = sp.sparse.linalg.expm(-1e8*((Sz_op)**2))
 G = fz + fsq + fn
@@ -26,11 +28,12 @@ print("diagonalizing")
 w, v =np.linalg.eigh(G.todense())
 idx = w.argsort()[::-1]   
 w = w[idx]
-if not os.path.isdir('./SymFrags/'):
-    os.mkdir('./SymFrags/')
-np.save("./SymFrags/BeH2_w", w)
+#Creating Directory for the molecule to store results, uncomment when needed
+#if not os.path.isdir('./SymFrags/'):
+#    os.mkdir('./SymFrags/')
+np.save("./SymFrags/'+mol+'_w", w)
 v = v[:,idx]
-np.save("./SymFrags/BeH2_v", v) # save unitary matrix 
+np.save("./SymFrags/'+mol+'_v", v) # save unitary matrix 
 print("starting counter")
 counter = 0
 for i in range(len(w)):
@@ -38,23 +41,23 @@ for i in range(len(w)):
         print((w[i]),i)
         counter += 1
 print(counter)
-np.save("./SymFrags/BeH2_counter", counter)
+np.save("./SymFrags/'+mol+'_counter", counter)
 vdag=np.conjugate(np.transpose(v))
-np.save("./SymFrags/BeH2_vdag", vdag)
+np.save("./SymFrags/'+mol+'_vdag", vdag)
 HamSpOp = of.linalg.get_sparse_operator(h_ferm)
 SymHam=np.matmul(vdag,np.matmul(HamSpOp.todense(),v))
 SymHam = SymHam[0:counter,0:counter]
-np.save("./SymFrags/BeH2_SymHam", SymHam) # save symmetry-adapted Hamiltonian
+np.save("./SymFrags/'+mol+'_SymHam", SymHam) # save symmetry-adapted Hamiltonian
 print(SymHam.shape)
 ExactEigs=of.eigenspectrum(h_ferm)
-np.save("./SymFrags/BeH2_gs", min(ExactEigs))
+np.save("./SymFrags/'+mol+'_gs", min(ExactEigs))
 TestEigs=np.linalg.eigvalsh(SymHam)
 print(min(ExactEigs))
 print(min(TestEigs))
 hf = openfermion.jw_hartree_fock_state(6,14) # corresponding to the molecule, ex. beh2
 SymHf =np.dot(vdag,hf)
 SymHf =np.copy(SymHf[0,0:counter])
-np.save("./SymFrags/BeH2_SymHf", SymHf) # save symmtery-adapted wavefunction
+np.save("./SymFrags/'+mol+'_SymHf", SymHf) # save symmtery-adapted wavefunction
 print(SymHf.shape)
 
 
