ef74fafcd30e74290f215c22a67f1d08a076933d
prathami11 <72123148+prathami11@users.noreply.github.com>
2023-12-06 06:31:02 +0530

    Update shrink_frags.py

--- CSA-master/CSA-master/shrink_frags.py
+++ CSA-master/CSA-master/shrink_frags.py
@@ -9,13 +9,12 @@ import time
 import pickle
 import saveload_utils as sl
 
-def shrink_frags(method):
+def shrink_frags(method, mol, index,n_qubits):
     start = time.time()
     print("Start")
-   
     print("Running for method:",method)
     # Importing Fragments
-    f = open("./Frag_Lib/"+method+"/beh2_"+method+"Frags", 'rb')
+    f = open("./Frag_Lib/"+method+"/"+mol+"_"+method+"Frags", 'rb')
     dict = pickle.load(f)
     f.close()
     ListFrags=dict # stores all the fragments
@@ -23,17 +22,21 @@ def shrink_frags(method):
     print(number_frags)
     for i in range(len(ListFrags)):
         print(i)
-        ListFrags[i] = openfermion.linalg.get_sparse_operator(ListFrags[i],14)
-    v = np.load("./SymFrags/BeH2_v.npy") # load the unitary matrix 
+        ListFrags[i] = openfermion.linalg.get_sparse_operator(ListFrags[i],n_qubits)
+    v = np.load("./SymFrags/BeH2_v.npy") # load the unitary matrix for the respective molecule
     vdag = np.load("./SymFrags/BeH2_vdag.npy")
     print("Shrinking")
     for i in range(len(ListFrags)):
         print(i)
         x = np.matmul(vdag,np.matmul(ListFrags[i].todense(),v))
-        np.save("./MatrixFrags/BeH2/GFRO/"+str(i), x[0:490,0:490])# change index value wrt the molecule
+        np.save("./MatrixFrags/BeH2/GFRO/"+str(i), x[0:index,0:index])# change index value wrt the molecule
     total_time = time.time()-start
     print("time in hrs",total_time/3600)
-
-shrink_frags("GFROLCU") #specify the method
+index = 490 # options 3 for h2, 105 for lih, 490 for beh2, 196 for h2o, 8008 for nh3
+mol = "beh2" # options: h2, lih, beh2, h2o, nh3
+h_ferm=sl.load_fermionic_hamiltonian(mol,path_prefix) #
+n_qubits = openfermion.count_qubits(h_ferm) # no. of qubits
+method = "GFROLCU" #options: FRO, GFRO, GFROLCU, LRLCU, svd, GCSASD
+shrink_frags(method,mol,index,n_qubits) #specify the method
 
 
