Adding PT calculation feature and fixing bugs in shrink_frags.py

CSA-master/CSA-master/shrink_frags.py
@@ -5,7 +5,7 @@ import openfermion
 path_prefix="../CSA-master/" ##Top of the project, corresponding to CSA/
 import sys
 sys.path.append(path_prefix)
-import time 
+import time
 import pickle
 import saveload_utils as sl
 
@@ -17,14 +17,14 @@ def shrink_frags(method, mol, index,n_qubits):
     f = open("./Frag_Lib/"+method+"/"+mol+"_"+method+"Frags", 'rb')
     dict = pickle.load(f)
     f.close()
-    ListFrags=dict # stores all the fragments
+    ListFrags=dict['grouping'] # stores all the fragments
     number_frags = len(ListFrags) # Number of fragments
     print(number_frags)
     for i in range(len(ListFrags)):
         print(i)
         ListFrags[i] = openfermion.linalg.get_sparse_operator(ListFrags[i],n_qubits)
-    v = np.load("./SymFrags/'+mol+'_v.npy") # load the unitary matrix for the respective molecule
-    vdag = np.load("./SymFrags/'+mol+'_vdag.npy")
+    v = np.load('./SymFrags/'+mol+'_v.npy') # load the unitary matrix for the respective molecule
+    vdag = np.load('./SymFrags/'+mol+'_vdag.npy')
     print("Shrinking")
     for i in range(len(ListFrags)):
         print(i)
@@ -32,11 +32,9 @@ def shrink_frags(method, mol, index,n_qubits):
         np.save("./MatrixFrags/"+mol+"/"+method+"/"+str(i), x[0:index,0:index])# change index value wrt the molecule
     total_time = time.time()-start
     print("time in hrs",total_time/3600)
-index = 490 # options 3 for h2, 105 for lih, 490 for beh2, 196 for h2o, 8008 for nh3
-mol = "beh2" # options: h2, lih, beh2, h2o, nh3
+index = 3 # options 3 for h2, 105 for lih, 490 for beh2, 196 for h2o, 8008 for nh3
+mol = "h2" # options: h2, lih, beh2, h2o, nh3
 h_ferm=sl.load_fermionic_hamiltonian(mol,path_prefix) #
 n_qubits = openfermion.count_qubits(h_ferm) # no. of qubits
-method = "GFROLCU" #options: FRO, GFRO, GFROLCU, LRLCU, svd, GCSASD
+method = "FRO" #options: FRO, GFRO, GFROLCU, LRLCU, svd, GCSASD
 shrink_frags(method,mol,index,n_qubits) #specify the method
-
-

CSA-master/CSA-master/shrink_ham.py
@@ -7,33 +7,35 @@ import saveload_utils as sl #From CSA library
 from scipy import sparse
 import os
 import pickle
-mol = "beh2" # options: h2, lih, beh2, h2o, nh3
+mol = "h2" # options: h2, lih, beh2, h2o, nh3
+n_elec = 2
+
 h_ferm=sl.load_fermionic_hamiltonian(mol,path_prefix) #loads the Openfermion Fermion operator corresponding to the
                                                       #molecule mol
-n_qubits = openfermion.count_qubits(h_ferm) # no. of qubits
-Sz_op = of.linalg.get_sparse_operator(of.hamiltonians.sz_operator(n_qubits/2))
-Sx_op = of.linalg.get_sparse_operator(of.hamiltonians.sx_operator(n_qubits/2))
-Sy_op = of.linalg.get_sparse_operator(of.hamiltonians.sy_operator(n_qubits/2))
+n_qubits = of.count_qubits(h_ferm) # no. of qubits
+Sz_op = of.linalg.get_sparse_operator(of.hamiltonians.sz_operator(n_qubits//2))
+Sx_op = of.linalg.get_sparse_operator(of.hamiltonians.sx_operator(n_qubits//2))
+Sy_op = of.linalg.get_sparse_operator(of.hamiltonians.sy_operator(n_qubits//2))
 # S squared operator
 S_sq = Sx_op**2+Sy_op**2+Sz_op**2
-n_op = of.linalg.get_sparse_operator(of.hamiltonians.number_operator(n_qubits),n_qubits) 
+n_op = of.linalg.get_sparse_operator(of.hamiltonians.number_operator(n_qubits),n_qubits)
 print("exponentiating")
 import scipy as sp
-n_elec = 6 # no. of ground state electrons of the respective molecule, modify accordingly
-fn =sp.sparse.linalg.expm(-1e8*((n_op-n_elec*sp.sparse.identity(2**n_qubit))**2))
+#n_elec = 2 # no. of ground state electrons of the respective molecule, modify accordingly
+fn =sp.sparse.linalg.expm(-1e8*((n_op-n_elec*sp.sparse.identity(2**n_qubits))**2))
 fsq =sp.sparse.linalg.expm(-1e8*((S_sq)**2))
 fz = sp.sparse.linalg.expm(-1e8*((Sz_op)**2))
 G = fz + fsq + fn
 print("diagonalizing")
 w, v =np.linalg.eigh(G.todense())
-idx = w.argsort()[::-1]   
+idx = w.argsort()[::-1]
 w = w[idx]
 #Creating Directory for the molecule to store results, uncomment when needed
 #if not os.path.isdir('./SymFrags/'):
 #    os.mkdir('./SymFrags/')
-np.save("./SymFrags/'+mol+'_w", w)
+np.save('./SymFrags/'+mol+'_w', w)
 v = v[:,idx]
-np.save("./SymFrags/'+mol+'_v", v) # save unitary matrix 
+np.save('./SymFrags/'+mol+'_v', v) # save unitary matrix
 print("starting counter")
 counter = 0
 for i in range(len(w)):
@@ -41,27 +43,21 @@ for i in range(len(w)):
         print((w[i]),i)
         counter += 1
 print(counter)
-np.save("./SymFrags/'+mol+'_counter", counter)
+np.save('./SymFrags/'+mol+'_counter', counter)
 vdag=np.conjugate(np.transpose(v))
-np.save("./SymFrags/'+mol+'_vdag", vdag)
+np.save('./SymFrags/'+mol+'_vdag', vdag)
 HamSpOp = of.linalg.get_sparse_operator(h_ferm)
 SymHam=np.matmul(vdag,np.matmul(HamSpOp.todense(),v))
 SymHam = SymHam[0:counter,0:counter]
-np.save("./SymFrags/'+mol+'_SymHam", SymHam) # save symmetry-adapted Hamiltonian
-print(SymHam.shape)
+np.save('./SymFrags/'+mol+'_SymHam', SymHam) # save symmetry-adapted Hamiltonian
+print("Shape of symmetric Hamiltonian:",SymHam.shape)
 ExactEigs=of.eigenspectrum(h_ferm)
-np.save("./SymFrags/'+mol+'_gs", min(ExactEigs))
+np.save('./SymFrags/'+mol+'_gs', min(ExactEigs))
 TestEigs=np.linalg.eigvalsh(SymHam)
 print(min(ExactEigs))
 print(min(TestEigs))
-hf = openfermion.jw_hartree_fock_state(6,14) # corresponding to the molecule, ex. beh2
+hf = of.jw_hartree_fock_state(n_elec,n_qubits) # corresponding to the molecule, ex. beh2
 SymHf =np.dot(vdag,hf)
 SymHf =np.copy(SymHf[0,0:counter])
-np.save("./SymFrags/'+mol+'_SymHf", SymHf) # save symmtery-adapted wavefunction
+np.save('./SymFrags/'+mol+'_SymHf', SymHf) # save symmtery-adapted wavefunction
 print(SymHf.shape)
-
-
-
-
-
-