cf6b145fdb27a73a7360a75021883fac896769d1
prathami11 <72123148+prathami11@users.noreply.github.com>
2023-12-15 13:39:43 -0800

    Update shrink_frags.py

--- CSA-master/CSA-master/shrink_frags.py
+++ CSA-master/CSA-master/shrink_frags.py
@@ -9,10 +9,14 @@ import time
 import pickle
 import saveload_utils as sl
 
-def shrink_frags(method, mol, index,n_qubits):
+def shrink_frags(method, mol,n_qubits):
     start = time.time()
     print("Start")
     print("Running for method:",method)
+    # Creating Directory for the molecule to store results, uncomment when needed
+    if not os.path.isdir('./MatrixFrags/'+mol):
+        os.mkdir('./MatrixFrags/'+mol+'/')
+        os.mkdir('./MatrixFrags/'+mol+'/'+method+'/')
     # Importing Fragments
     f = open("./Frag_Lib/"+method+"/"+mol+"_"+method+"Frags", 'rb')
     dict = pickle.load(f)
@@ -25,6 +29,7 @@ def shrink_frags(method, mol, index,n_qubits):
         ListFrags[i] = openfermion.linalg.get_sparse_operator(ListFrags[i],n_qubits)
     v = np.load('./SymFrags/'+mol+'_v.npy') # load the unitary matrix for the respective molecule
     vdag = np.load('./SymFrags/'+mol+'_vdag.npy')
+    index = np.load('./SymFrags/'+mol+'_counter.npy')
     print("Shrinking")
     for i in range(len(ListFrags)):
         print(i)
@@ -32,9 +37,10 @@ def shrink_frags(method, mol, index,n_qubits):
         np.save("./MatrixFrags/"+mol+"/"+method+"/"+str(i), x[0:index,0:index])# change index value wrt the molecule
     total_time = time.time()-start
     print("time in hrs",total_time/3600)
-index = 3 # options 3 for h2, 105 for lih, 490 for beh2, 196 for h2o, 8008 for nh3
+if not os.path.isdir('./MatrixFrags/'):
+    os.mkdir('./MatrixFrags/')
 mol = "h2" # options: h2, lih, beh2, h2o, nh3
 h_ferm=sl.load_fermionic_hamiltonian(mol,path_prefix) #
 n_qubits = openfermion.count_qubits(h_ferm) # no. of qubits
 method = "FRO" #options: FRO, GFRO, GFROLCU, LRLCU, svd, GCSASD
-shrink_frags(method,mol,index,n_qubits) #specify the method
+shrink_frags(method,mol,n_qubits) #specify the method
